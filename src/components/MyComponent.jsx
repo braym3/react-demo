340579f8dfdea1589ed2322bf9cf8c15621fce87
braym3 <60962768+mbray3@users.noreply.github.com>
2023-04-27 14:25:12 +0100

    Added basic component

---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,9 @@
+const MyComponent = () => (
+    <div className="content">
+        <div>
+            <h1 className="component-title">Cats</h1>
+        </div>
+    </div>
+);
+
+export default MyComponent;
